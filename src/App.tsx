d4277e0ae840db66b1d56fb02c656431bbd2366d
Vincent Zheng <vzheng147@gmail.com>
2025-06-10 14:57:49 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Banner from "./banner/Banner";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Banner></Banner>
+    </>
+  );
+}
+
+export default App;
---
+++ src/banner/Banner.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div class="banner">
+      <div>Drone Agr</div>
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Banner;
